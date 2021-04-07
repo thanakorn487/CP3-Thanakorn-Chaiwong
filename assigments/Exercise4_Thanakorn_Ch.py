ac32001f2a4617dd67edb3552510801522514128
thanakorn487 <derst487@gmail.com>
2021-04-07 10:14:11 +0700

    Start Coding Exercise 4

---
+++ assigments/Exercise4_Thanakorn_Ch.py
@@ -0,0 +1,9 @@
+Score_FE = 88 #Foundation English
+Score_GB = 81 #General Business
+Score_ICS = 92 #Introduction to Computer Systems
+Score_CP = 97 #Computer Programming
+print("---Your Score---")
+print("Foundation English : ",Score_FE)
+print("General Business : ",Score_GB)
+print("Introduction to Computer Systems : ",Score_ICS)
+print("Computer Programming : ",Score_CP)
