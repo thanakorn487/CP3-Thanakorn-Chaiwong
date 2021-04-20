42389abd36fa23ba5a0423941578e4a8a558e046
thanakorn487 <derst487@gmail.com>
2021-04-20 11:47:10 +0700

    create function

---
+++ assigments/Lecture50_Thanakorn_Ch.py
@@ -0,0 +1,14 @@
+def AddNumber(x,y):
+    print(x, "+", y, "=", x + y)
+def SubtractNumber(x,y):
+    print(x, "-", y, "=", x - y)
+def MultiplyNumber(x,y):
+    print(x, "x", y, "=", x * y)
+def DivideNumber(x,y):
+    print(x, "/", y, "=", x / y)
+a = int(input("Input x : "))
+b = int(input("Input y : "))
+AddNumber(a,b)
+SubtractNumber(a,b)
+MultiplyNumber(a,b)
+DivideNumber(a,b)
